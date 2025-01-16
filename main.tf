153ef44b10303a8fbb75b86e75bf86c5a63e4bf8
David G <48044383+gradav@users.noreply.github.com>
2025-01-16 10:34:13 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,18 +1,3 @@
-# Declare GOOGLE_CREDENTIALS as a variable
-variable "GOOGLE_CREDENTIALS" {
-  type        = string
-  description = "The JSON credentials for the Google Cloud provider."
-  sensitive   = true
-}
-
-# Configure the Google provider to use the credentials
-provider "google" {
-  project     = "terraform-447821"  # Ensure this matches your project ID
-  region      = "us-west1"
-  zone        = "us-west1-a"
-  credentials = var.GOOGLE_CREDENTIALS
-}
-
 # Fetch the most recent Ubuntu image
 data "google_compute_image" "ubuntu" {
   most_recent = true
