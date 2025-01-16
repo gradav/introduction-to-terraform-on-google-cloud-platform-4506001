9d60a421c32ec25651abec296679609ec648f0cc
David G <48044383+gradav@users.noreply.github.com>
2025-01-16 10:34:49 -0600

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,6 +1,5 @@
-#provider "google" {
-#  project = "terraform-447821"
-#  region  = "us-west1"
-#  zone    = "us-west1-a"
-#  credentials = var.GOOGLE_CREDENTIALS
-#}
+provider "google" {
+  project = "terraform-445119"
+  region  = "us-west1"
+  zone    = "us-west1-a"
+}
